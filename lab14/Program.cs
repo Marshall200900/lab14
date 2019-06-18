888335725c87b0f5e8fa2f9742eec5ea1b9e3643
Арсений Сидоров <aasidorov_6@edu.hse.ru>
2019-06-18 16:00:21 +0500

    still isnt working

--- lab14/Program.cs
+++ lab14/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShopLibrary;
 
 namespace lab14
 {
@@ -10,6 +11,45 @@ namespace lab14
     {
         static void Main(string[] args)
         {
+            ReqCollection shop = new ReqCollection(10);
+            shop.Generate(5);
+
+            ReqCollection shop2 = new ReqCollection(10);
+            shop2.Generate(5);
+
+            Console.WriteLine("Коллекция shop");
+            shop.Show();
+            Console.WriteLine("----------------------------------------------------");
+
+            Console.WriteLine("Коллекция shop");
+            shop.Show();
+            Console.WriteLine("----------------------------------------------------");
+
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("Наименование всех товаров в заданном отделе магазина.");
+            foreach(var a in shop.NamesOfGoods("молочный"))
+            {
+                Console.WriteLine(a);
+            }
+            Console.Write("Количество товаров в молочном отделе: ");
+            Console.WriteLine(shop.GetNumberOfGoods("молоко"));
+
+            Console.WriteLine("Пересечение");
+            foreach (var a in shop.Intersect(shop2))
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("Исключение");
+            foreach (var a in shop.Except(shop2))
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("Объединение");
+            foreach (var a in shop.Concat(shop2))
+            {
+                Console.WriteLine(a);
+            }
+
         }
     }
 }
---
+++ lab14/ReqCollection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopLibrary;
+
+namespace lab14
+{
+    class ReqCollection:MySortedDictionary<int, Goods>
+    {
+
+        public void Generate(int num)
+        {
+            
+            for(int i = 0; i < num; i++)
+            {
+                switch (i / 3)
+                {
+                    case 0:
+                        Add(i, new Toy(true));
+                        break;
+                    case 1:
+                        Add(i, new MilkProduct(true));
+                        break;
+                    case 2:
+                        Add(i, new Product(true));
+                        break;
+                }
+            }
+        }
+        public ReqCollection(int cap):base(cap)
+        {
+
+        }
+        public List<Goods> Intersect(ReqCollection list2)
+        {
+            List<Goods> intersect = (from c in this select c.Value).Intersect(from c in list2 select c.Value).ToList();
+            return intersect;
+        }
+
+        public List<Goods> Except(ReqCollection list2)
+        {
+            List<Goods> except = (from c in this select c.Value).Except(from c in list2 select c.Value).ToList();
+            return except;
+        }
+        public List<Goods> Concat(ReqCollection list2)
+        {
+            List<Goods> concat = (from c in this select c.Value).Concat(from c in list2 select c.Value).ToList();
+            return concat;
+        }
+        public List<string> NamesOfGoods(string department)
+        {
+            return (from c in this where c.Value.Department == department select c.Value.Name).ToList();
+        }
+        public int GetNumberOfGoods(string name)
+        {
+            return (from c in this where c.Value.Name == name select c.Value).Count();
+        }
+        public double GetPriceByName(string name)
+        {
+            double[] prices = (from c in this select c.Value.Price).ToArray();
+            double sum = prices.Aggregate((a, b) => a + b);
+            return sum;
+        }
+
+        //IEnumerator<Goods> IEnumerable<Goods>.GetEnumerator()
+        //{
+        //    return Values.GetEnumerator();
+        //}
+    }
+}
